869c58eeb035edda768675eb76906782b17725a6
SimoneDUrso <simone.durso98@gmail.com>
2024-09-06 16:32:39 +0200

    [UPD] Added if check on backend side and if check inside "p" tag. Milestone 1 completed.

--- index.php
+++ index.php
@@ -9,6 +9,18 @@ function generatePassword($length)
     }
     return $password;
 }
+
+// Controllo se esiste il parametro "length"
+if (isset($_GET['length'])) {
+
+    // Qui converto il parametro in un numero intero
+    $length = intval($_GET['length']);
+
+    // Se la password è compresa tra 4 e 30 generami la password
+    if ($length >= 4 && $length <= 30) {
+        $generatedPassword = generatePassword($length);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +31,7 @@ function generatePassword($length)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Strong Password Generator</title>
 </head>
 
 <body>
@@ -36,7 +48,19 @@ function generatePassword($length)
                 </form>
             </div>
             <div class="col-6">
-                <p>ciao</p>
+                <p>
+                    <?php
+                    // Se generatedPassword esiste e le condizioni sono state rispettate restituiscimi la password a schermo
+                    if (isset($generatedPassword)) {
+                        echo "<p class='alert alert-info'>Password Generata: $generatedPassword</p>";
+                    }
+
+                    // Altrimenti restituiscimi questo messaggio di avviso
+                    else {
+                        echo "<div>La lunghezza deve essere tra 4 e 30 caratteri.</div>";
+                    }
+                    ?>
+                </p>
             </div>
         </div>
     </div>
